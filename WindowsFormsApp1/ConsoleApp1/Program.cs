2887c844cabc05f4135b63b1fb662b9e1a808f8e
Andrew Seleznjov <Seleznjov@gmail.com>
2020-12-21 15:43:30 +0200

    Push new

---
+++ WindowsFormsApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ApexTech.MyPrograms;
+
+namespace ConsoleApp1
+{
+    public enum MyEnum
+    {
+       Monday, Tusday
+        };
+    class Program
+    {
+        MyEnum p = MyEnum.Monday;
+    }
+}
+
+namespace ApexTech.MyPrograms
+{
+    class MyClass
+    {
+        
+
+    }
+}
---
+++ WindowsFormsApp1/EnumProgramm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EnumProgramm
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+        static void Main(string[] args)
+        {
+
+            WeekDays day = WeekDays.Monday;
+            Console.WriteLine(day);
+            Console.WriteLine(WeekDays);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
